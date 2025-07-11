f15df8ea1cb7ec367762baffe83effa90531a9c6
wahyuumaternate <retmujago@gmail.com>
2025-07-11 15:07:39 +0900

    Breadcrumbs delet

--- resources/views/themes/medicio/detail_post.blade.php
+++ resources/views/themes/medicio/detail_post.blade.php
@@ -239,14 +239,6 @@
 @section('main')
     <main id="main">
         <!-- Breadcrumbs -->
-        <section class="breadcrumbs">
-            <div class="container">
-                <div class="d-flex justify-content-between align-items-center">
-                    <h2>{{ $page->title }}</h2>
-
-                </div>
-            </div>
-        </section>
 
         <!-- Article Section -->
         <section class="inner-page">
